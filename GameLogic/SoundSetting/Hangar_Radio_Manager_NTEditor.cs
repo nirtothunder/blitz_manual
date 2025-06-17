c0203559710cbbbcc0fce4e650cb26dde1d225f1
nirtothunder <wusrusgd@gmail.com>
2025-06-17 17:21:41 +0700

    Update Hangar_Radio_Manager_NTEditor.cs

--- GameLogic/SoundSetting/Hangar_Radio_Manager_NTEditor.cs
+++ GameLogic/SoundSetting/Hangar_Radio_Manager_NTEditor.cs
@@ -1,40 +1,43 @@
-using UnityEngine;
 using UnityEditor;
+using UnityEngine;
 
 namespace nirtothunder
 {
     [CustomEditor(typeof(Hangar_Radio_Manager_NT))]
-    public class Hangar_Radio_Manager_NTEditor : Editor
+    public class Hangar_Radio_Manager_NT_Editor : Editor
     {
         SerializedProperty Hangar_Music_PlayerProp;
         SerializedProperty Radio_Music_SourceProp;
         SerializedProperty Fade_DurationProp;
-        SerializedProperty Activation_KeyProp;
+        SerializedProperty Hangar_Music_DelayProp;
 
         void OnEnable()
         {
             Hangar_Music_PlayerProp = serializedObject.FindProperty("Hangar_Music_Player");
             Radio_Music_SourceProp = serializedObject.FindProperty("Radio_Music_Source");
             Fade_DurationProp = serializedObject.FindProperty("Fade_Duration");
-            Activation_KeyProp = serializedObject.FindProperty("Activation_Key");
+            Hangar_Music_DelayProp = serializedObject.FindProperty("Hangar_Music_Delay");
         }
 
         public override void OnInspectorGUI()
         {
             serializedObject.Update();
 
-            GUI.backgroundColor = new Color(0.8f, 0.8f, 0.2f, 1.0f);
+            EditorGUILayout.Space();
+            EditorGUILayout.HelpBox("Управление радио в ангаре\nПервое нажатие: Включает радио\nВторое нажатие: Выключает радио (музыка ангара включится через указанную задержку)", MessageType.Info);
 
             EditorGUILayout.Space();
-            EditorGUILayout.HelpBox("Контроллер радио кнопки", MessageType.None, true);
-            
-            EditorGUILayout.PropertyField(Hangar_Music_PlayerProp);
-            EditorGUILayout.PropertyField(Radio_Music_SourceProp);
-            EditorGUILayout.PropertyField(Fade_DurationProp);
-            EditorGUILayout.PropertyField(Activation_KeyProp);
+            EditorGUILayout.LabelField("Основные настройки", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(Hangar_Music_PlayerProp, new GUIContent("Плеер музыки ангара"));
+            EditorGUILayout.PropertyField(Radio_Music_SourceProp, new GUIContent("Источник радио"));
 
             EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Настройки переходов", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(Fade_DurationProp, new GUIContent("Длительность fade эффекта"));
+            EditorGUILayout.PropertyField(Hangar_Music_DelayProp, new GUIContent("Задержка включения музыки ангара (сек)"));
+
             EditorGUILayout.Space();
+            EditorGUILayout.HelpBox("Для работы требуется:\n1. Коллайдер на объекте\n2. Назначенные аудио источники", MessageType.Warning);
 
             serializedObject.ApplyModifiedProperties();
         }
